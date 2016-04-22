edff490c04cc23073b62d824ce6f00f845dda1b2
dragonslayern <lovelidberg@hotmail.com>
2016-04-22 17:13:09 +0200

    prefabs and scripts

---
+++ Assets/JoinServer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoinServer : MonoBehaviour, ICardboardGazeResponder  {
+
+	public TextMesh text;
+
+	void Start () {
+		
+		GetComponent<Renderer>().material.color = Color.red;
+
+	}
+
+	public void joinServer(){
+
+		PhotonNetwork.JoinRoom (text.text);
+
+
+	}
+
+	#region ICardboardGazeResponder implementation
+
+	// Called when the Cardboard trigger is used, between OnGazeEnter
+	/// and OnGazeExit.
+	 	public void OnGazeEnter() {
+
+
+	}
+
+	/// Called when the user stops looking on the GameObject, after OnGazeEnter
+	/// was already called.
+	public void OnGazeExit() {
+
+
+
+
+	}
+	public void OnGazeTrigger() {
+
+		joinServer();
+	}
+
+	#endregion
+
+}
