d83ab5dada4993e41a5d8eb296bbfa25f4549913
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-04-27 16:36:17 +0200

    Add tower game constans - turn durations etc.

---
+++ Assets/Scripts/Custom/Game/TowerGameConstants.cs
@@ -0,0 +1,12 @@
+namespace TowerVR
+{
+    public class TurnTimeLimits
+    {
+        public const float SelectingTowerPiece = 10.0f;
+        public const float PlacingTowerPiece = 20.0f;
+        public const float TowerReacting = 30.0f;
+        
+        // No instantiation of class
+        private TurnTimeLimits() {}
+    }
+}
