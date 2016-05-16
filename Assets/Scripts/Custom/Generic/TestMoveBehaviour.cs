6225e366587651eca88f1cf8723ee0997d9d2a35
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-05-16 14:20:24 +0200

    Create wisp player object with color depending on player ID

---
+++ Assets/Scripts/Custom/Generic/TestMoveBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMoveBehaviour : MonoBehaviour {
+	private Vector3 initialPosition;
+	
+	public float deltaX = 15, deltaY = 15;
+	
+	void Start()
+	{
+		initialPosition = transform.position;
+	}
+	
+	void Update () 
+	{
+		var position = initialPosition;
+		
+		position.x += deltaX * Mathf.Cos(Time.time);
+		position.y += deltaY * Mathf.Sin(Time.time);
+		
+		transform.position = position;
+	}
+}
