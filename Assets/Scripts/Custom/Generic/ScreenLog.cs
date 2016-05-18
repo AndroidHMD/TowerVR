f071d32c468c00a33ea84c4a92d8dc1e7464badd
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-05-18 17:10:16 +0200

    Create logging class that logs to a screen GUI

---
+++ Assets/Scripts/Custom/Generic/ScreenLog.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScreenLog : MonoBehaviour {
+	private static List<string> entries = new List<string>();
+	
+	public static void Log(object obj)
+	{
+		entries.Add(obj.ToString());
+	}
+	
+	void OnGUI()
+	{
+		foreach (var entry in entries)
+		{
+			GUILayout.Label(entry);
+		}
+	}
+	
+	void Start()
+	{
+		Log(""); Log(""); Log(""); Log(""); Log(""); Log(""); Log(""); Log(""); Log(""); Log(""); Log(""); Log(""); Log(""); Log("");
+	}
+}
