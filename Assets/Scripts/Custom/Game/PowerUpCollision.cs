3016385e9ba12f3840f25de66137e53bcd0a34c5
dragonslayern <lovelidberg@hotmail.com>
2016-05-19 14:30:00 +0200

    merged!

---
+++ Assets/Scripts/Custom/Game/PowerUpCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+
+		col.SendMessage ("HitPowerUp");
+	
+		Destroy (col.gameObject);
+		Destroy (gameObject);
+
+	}
+}
