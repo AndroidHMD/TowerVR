528edbd75dbb6d76a6d2a4329be174a22a59d59a
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-05-23 10:46:06 +0200

    Generate player names from 20^3 combinations

---
+++ Assets/Scripts/Custom/Game/PlayerNameChooser.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerNameChooser : MonoBehaviour {
+	public Text text;
+
+	void Start () {
+		GenerateAndSetPlayerName();
+	}
+	
+	private void GenerateAndSetPlayerName()
+	{
+		string name = PlayerNameGenerator.GenerateName();
+		text.text = name;
+		PhotonNetwork.playerName = name;
+	}
+}
---
+++ Assets/Scripts/Custom/Game/PlayerNameGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNameGenerator {
+	public static string[] prefixes = new string[]{
+		"Mr.", "Mrs.", "Dr.", "Professor", 
+		"Little", "Gigantic", "Fierce", "Tiny", "Enormous",
+		"Angry", "Calm", "Crazy", "Idiotic", "Drunken",
+		"Swaggy", "Derpy", "Explosive", "Destructive", "Masochistic",
+		"King", "Queen", "Prince", "Princess"
+	};
+	
+	public static string[] mainNames = new string[]{
+		"Boom", "Potato", "Carrot",
+		"Rabbit", "Bunny", "Elk", "Moose", "Bird", "Dog", "Cat",
+		"Master", "Noob", "Pro", "Newbie",
+		"Robot", "Machine", 
+		"Carl Gustaf", "Obama"
+	};
+	
+	public static string[] suffixes = new string[]{
+		"the Great", "the First", "the Second", "the Third",
+		"I", "II", "III", "IV", "V", "VI", "XII", "XIV", "XVI",
+		"Jr.", "Sr.",
+		"PhD", "MSc", "BSc"
+	};
+	
+	public static string GenerateName()
+	{
+		string prefix = GetRandomInArray(prefixes);
+		string main = " " + GetRandomInArray(mainNames);
+		
+		// generate a suffix 50% of the time
+		string suffix = "";
+		if (Random.Range(0, 2) == 1)
+		{
+			suffix = " " + GetRandomInArray(suffixes);
+		}
+		
+		return prefix + main + suffix;
+	}
+	
+	private static string GetRandomInArray(string[] arr)
+	{
+		int index = Random.Range(0, arr.Length);
+		return arr[index];
+	}
+}
