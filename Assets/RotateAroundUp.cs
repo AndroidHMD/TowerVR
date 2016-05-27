8d09f1ca97bcc6752a1c7f8ad2c233523a93f382
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-05-27 15:53:02 +0200

    Add waiting for players text in lobby with animated dots...

---
+++ Assets/RotateAroundUp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAroundUp : MonoBehaviour 
+{
+	public Vector3 deltaRotationAxis = Vector3.zero;
+	public float revolutionsPerSecond = 0.5f;
+	
+	private Vector3 startPosition;
+	
+	void Start()
+	{
+		startPosition = transform.position;
+	}
+	
+	void Update () 
+	{
+		transform.RotateAround(startPosition + deltaRotationAxis, Vector3.up, 360 * revolutionsPerSecond * Time.deltaTime);
+	}
+}
---
+++ Assets/WaitingForPlayers.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitingForPlayers : MonoBehaviour 
+{
+	public TextMesh textMesh;
+	public string text = "Waiting for players";
+	
+	public float dotsPerSecond = 3.0f;
+	
+	private float startTime = 0.0f;
+	
+	void Start()
+	{
+		startTime = Time.time;
+	}
+	
+	void Update () 
+	{
+		int second = (int) ((Time.time - startTime) * dotsPerSecond);
+		
+		int nDots = second % 4;
+		
+		string dots = "";
+		for (int i = 0; i < nDots; ++i)
+		{
+			dots += '.';
+		}
+		
+		textMesh.text = text + dots;
+	}
+}
